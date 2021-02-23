ee352d65cf566395e6e72276b7b6b98f6be3725d
Kevin Xu <58110712+kevinxuht@users.noreply.github.com>
2021-02-23 01:27:33 +0000

    exp-weighted cov estimate

---
+++ ewm_cov.py
@@ -0,0 +1,94 @@
+import warnings
+import pandas as pd
+import numpy as np
+import scipy as sp
+import pandas_datareader as pd_data
+
+start_date = '2015-09-01'
+end_date = '2015-12-31'
+
+symbols = ['AAPL','GS', 'GC=F', 'GE']#'TSLA'
+df = pd_data.DataReader(symbols,'yahoo',start_date,end_date)['Adj Close']
+
+df = df.dropna()
+dfrtn = np.log(df).diff().dropna()
+
+def returns_from_prices(prices, log_returns=False):
+    """
+    Calculate the returns given prices.
+
+    :param prices: adjusted (daily) closing prices of the asset, each row is a
+                   date and each column is a ticker/id.
+    :type prices: pd.DataFrame
+    :param log_returns: whether to compute using log returns
+    :type log_returns: bool, defaults to False
+    :return: (daily) returns
+    :rtype: pd.DataFrame
+    """
+    if log_returns:
+        return np.log(prices).diff().dropna(how="all")
+    else:
+        return prices.pct_change().dropna(how="all")
+        
+def _pair_exp_cov(X, Y, span=180):
+    """
+    Calculate the exponential covariance between two timeseries of returns.
+
+    :param X: first time series of returns
+    :type X: pd.Series
+    :param Y: second time series of returns
+    :type Y: pd.Series
+    :param span: the span of the exponential weighting function, defaults to 180
+    :type span: int, optional
+    :return: the exponential covariance between X and Y
+    :rtype: float
+    """
+    covariation = (X - X.mean()) * (Y - Y.mean())
+    # Exponentially weight the covariation and take the mean
+    if span < 10:
+        warnings.warn("it is recommended to use a higher span, e.g 30 days")
+    return covariation.ewm(span=span).mean().iloc[-1]     
+
+def exp_cov(prices, returns_data=False, span=180, frequency=252, **kwargs):
+    """
+    Estimate the exponentially-weighted covariance matrix, which gives
+    greater weight to more recent data.
+
+    :param prices: adjusted closing prices of the asset, each row is a date
+                   and each column is a ticker/id.
+    :type prices: pd.DataFrame
+    :param returns_data: if true, the first argument is returns instead of prices.
+    :type returns_data: bool, defaults to False.
+    :param span: the span of the exponential weighting function, defaults to 180
+    :type span: int, optional
+    :param frequency: number of time periods in a year, defaults to 252 (the number
+                      of trading days in a year)
+    :type frequency: int, optional
+    :return: annualised estimate of exponential covariance matrix
+    :rtype: pd.DataFrame
+    """
+    if not isinstance(prices, pd.DataFrame):
+        warnings.warn("data is not in a dataframe", RuntimeWarning)
+        prices = pd.DataFrame(prices)
+    assets = prices.columns
+    if returns_data:
+        returns = prices
+    else:
+        returns = returns_from_prices(prices)
+    N = len(assets)
+
+    # Loop over matrix, filling entries with the pairwise exp cov
+    S = np.zeros((N, N))
+    for i in range(N):
+        for j in range(i, N):
+            S[i, j] = S[j, i] = _pair_exp_cov(
+                returns.iloc[:, i], returns.iloc[:, j], span
+            )
+    cov = pd.DataFrame(S * frequency, columns=assets, index=assets)
+    
+    return cov
+
+
+exp_cov(df)
+exp_cov(dfrtn, returns_data = True)
+
